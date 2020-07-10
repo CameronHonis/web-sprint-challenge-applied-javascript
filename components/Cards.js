e32e0b28618b5b6340938eeeceb69ab053d82bc0
unknown <cameron-04@hotmail.com>
2020-07-10 12:52:38 -0400

    cameron-honis

--- components/Cards.js
+++ components/Cards.js
@@ -17,6 +17,48 @@
 //   </div>
 // </div>
 //
-// Add a listener for click events so that when a user clicks on a card, the headline of the article is logged to the console.
+// Add a listener for click events so that when a user clicks on a card, the headline of the article 
+//      is logged to the console.
 //
 // Use your function to create a card for each of the articles, and append each card to the DOM.
+function createCard(topic,obj){
+    let card = document.createElement('div')
+    card.className = 'card'
+    card.classList.add(topic === 'node' ? 'node.js' : topic)
+    let headline = document.createElement('div')
+    card.appendChild(headline)
+    headline.className = 'headline'
+    headline.textContent = obj.headline
+    let author = document.createElement('div')
+    card.appendChild(author)
+    author.className = 'author'
+    let imgcont = document.createElement('div')
+    author.appendChild(imgcont)
+    imgcont.className = 'img-container'
+    let img = document.createElement('img')
+    imgcont.appendChild(img)
+    img.src = obj.authorPhoto
+    let span = document.createElement('span')
+    author.appendChild(span)
+    span.textContent = obj.authorName
+    return card
+}
+const testData = {
+    data: {
+        articles: {
+            one: [{authorName: 'one',authorPhoto: './assets/fido.jpg',headline: 'test'}]
+        }
+    }
+}
+
+axios.get('https://lambda-times-backend.herokuapp.com/articles')
+    .then(obj => {
+        for (let k in obj.data.articles){
+            for (let i = 0; i < obj.data.articles[k].length; i++){
+                document.querySelector('.cards-container').appendChild(createCard(k.toLowerCase(),obj.data.articles[k][i]))
+            }
+        }
+    })
+    .catch(obj => {
+        document.querySelector('#error').classList.remove('hidden')
+    })
